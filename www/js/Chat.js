6e0e13ca95c4598032a42e363dbc216be33ca74e
ropoble <robin.poble@etu.uca.fr>
2024-03-29 01:51:15 +0100

    Le Chat fonctionne PogChamp

--- www/js/Chat.js
+++ www/js/Chat.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script);
 
 
@@ -8,26 +8,77 @@ message_log = document.getElementById("Chat_list")
 body = document.getElementById("Chat_list");
 input = document.getElementById("Type_box");
 form = document.getElementById("Message_send");
+box = document.getElementById("Type_box");
+
 
 form.addEventListener("submit",function(event) {
-    sendMessage(event);
-})
-
-function sendMessage(event) {
     event.preventDefault();
-    req=$.ajax({url: "../htbin/chatsend.py", method:"POST",success: function(data) {
-        console.log(req.responseText);
-        items=req.responseText
-        if (items[num]!=0) {
-            error_log.innerHTML = items[msg];
-        }
-        else {
-            req2=$.getJSON("../htbin/chatget.py", function(data) {
-                message_log.innerHTML=req2.responseText;
-            })
+    sendMessage();
+});
+
+
+var intervalID = window.setInterval(function() {
+    req2=$.ajax({url: "../htbin/chatget.py", dataType: "JSON", success: function(rep2) {
+        for (let i=rep2.length-1; i<rep2.length; i++) {
+            cur=rep2[i];
+            if (body.lastChild.lastChild!=null && !(cur.msg==body.lastChild.lastChild.innerHTML && cur.user==body.lastChild.firstChild.innerHTML)) {
+                fragment = document.createDocumentFragment();
+                msg_c= fragment.appendChild(document.createElement("div"));
+                msg_c.className="Message_container"
+                usr= msg_c.appendChild(document.createElement("div"))
+                msg=msg_c.appendChild(document.createElement("div"))
+                usr.innerHTML=cur.user;
+                msg.innerHTML=cur.msg;
+                body.appendChild(fragment);
+            }
+            
         }
     }
     })
-}
+},1000);
 
 
+
+$(function() {
+    $('form').each(function() {
+        $(this).find('input').keypress(function(e) {
+            // Enter pressed?
+            if(e.which == 10 || e.which == 13) {
+                sendMessage(event);
+            }
+        });
+
+        $(this).find('input[type=submit]').hide();
+    });
+});
+
+function sendMessage(event) {
+    event.preventDefault();
+    console.log(box.value);
+    
+    req=$.ajax({url: "../htbin/chatsend.py", method: "POST", data:{msg: box.value} ,success: function(rep) {
+        console.log(req.responseText);
+        if (rep.num!=0) {
+            error_log.innerHTML = rep.msg;
+            console.log(typeof(rep)+"\n"+rep.num+"\n"+rep.msg);
+        }
+        else {
+           req2=$.ajax({url: "../htbin/chatget.py", dataType: "JSON", success: function(rep2) {
+                for (let i=rep2.length-1; i<rep2.length; i++) {
+                    cur=rep2[i];
+                    fragment = document.createDocumentFragment();
+                    msg_c= fragment.appendChild(document.createElement("div"));
+                    msg_c.className="Message_container"
+                    usr= msg_c.appendChild(document.createElement("div"))
+                    msg=msg_c.appendChild(document.createElement("div"))
+                    usr.innerHTML=cur.user;
+                    msg.innerHTML=cur.msg;
+                    body.appendChild(fragment);
+                    
+                }
+           }
+        })
+        }
+    }
+    })
+};
